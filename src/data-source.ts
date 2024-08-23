55daaa9f1dba81381c4f4cc84784c5af2c714259
notsanchez <dznextreme@gmail.com>
2024-08-23 02:47:19 -0300

    dotenv

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,18 @@
+require('dotenv').config()
+
 import { DataSource } from 'typeorm';
 import { Contact } from './entities/Contact';
+import { Message } from './entities/Message';
 import { User } from './entities/User';
 
 export const AppDataSource = new DataSource({
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'your-username',
-    password: 'your-password',
-    database: 'your-database',
-    entities: [User, Contact],
-    //synchronize: true,
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [User, Contact, Message],
+    synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
     logging: false,
 });
